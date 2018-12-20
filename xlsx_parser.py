c9febec86815ff3ccced22c3cb6f6438ce8b2dbe
Ellen Oldenburg <Ellen@Ellens-Air-2.Speedport_W723_V_Typ_A_1_01_018>
2018-12-20 18:41:37 +0100

    finished check_if_float() // opens csv and checks a column

--- xlsx_parser.py
+++ xlsx_parser.py
@@ -6,18 +6,16 @@ import os
 
 class Parser:
 
-    def __init__(self, file_name, sheet_name):
-        self.file_name = file_name
-        self.sheet_name = sheet_name
+    def __init__(self):
         self.csv_file = ""
 
-        
+        if not os.path.exists("converted_files"):
+            os.makedirs("converted_files")
 
-
-    def excel_to_csv(self):
-        wb = xlrd.open_workbook(self.file_name)
-        sh = wb.sheet_by_name(self.sheet_name)
-        self.csv_file = open('converted_files/{}_{}.csv'.format(self.file_name, datetime.date.today()), 'w')
+    def excel_to_csv(self, file_name, sheet_name):
+        wb = xlrd.open_workbook(file_name)
+        sh = wb.sheet_by_name(sheet_name)
+        self.csv_file = open('converted_files/{}_{}.csv'.format(file_name, datetime.date.today()), 'w')
         wr = csv.writer(self.csv_file, quoting=csv.QUOTE_ALL)
 
         for rownum in range(sh.nrows):
@@ -28,7 +26,7 @@ class Parser:
         os.system("open converted_files/")
 
         print("\n")
-        print("[Done] {} was successfully converted to CSV".format(self.file_name))
+        print("[Done] {} was successfully converted to CSV".format(file_name))
         print("[Warning] It needs up to 10 seconds until the file is created!")
 
     def delete_null(self):
@@ -36,3 +34,19 @@ class Parser:
         with open(self.csv_file, 'r') as input_file:
             content = csv.reader(input_file)
             print(content)
+
+    def check_if_float(self, filename, column):
+        with open(filename, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f, delimiter=',')
+
+            column_content = [row[column] for row in reader]
+            print(column_content[:10], "\n")
+
+            for i,e in enumerate(column_content):
+                try:
+                    float(e)
+                except:
+                    print("[Warning] Column contains words/strings or empty!")
+                    print("Column: {}".format(i))
+
+
