81952c060d642fc075031c89e5f9da489ee4ffd0
Alexander Teusz <teusz.alexander@gmail.com>
2018-12-24 22:13:17 +0100

    better help message

--- menu.py
+++ menu.py
@@ -12,34 +12,61 @@ class Menu:
  #     # #######  #####  #     #    #       #    # #    #  ####  ###### #    # 
 
  by Alexander Teusz and Ellen Oldenburg
-
-        GENERAL INFO:
-        You can type in your command using the command line. The numbers in the menu below are used to start a command.
-
-        e.g.:
-        -> 1 
-        
-        --- Just type in an emptry string to take a look at the menu --- 
-
         """)
 
     def launch_menu(self):
         print("""
-        ### MENU ###
+        ---------- MENU ----------
         
         (1)\t\t convert to CSV & opens directory
-        (2)\t\t delete null cells
+        (2)\t\t delete rows with null in a specific cell
         (3)\t\t check if column only has numbers
         (4)\t\t change string in column with number ('Hi' -> 1)
         (5)\t\t get the max value of a specific column
         (6)\t\t get the ages of all persons
+        (7)\t\t count a value in a column
         
         (help)\tshows help information
+        
         (exit)\twill end the parser 
         """)
 
     def show_help(self):
         print("""
-        file_name:  needs to be a *.xlsx e.g. file.xlsx
-        sheet_name: the specific sheet in the excel workbook (the table name)
+        Usage: python app.py 
+
+            Simple program to analyze and manipulate Excel and CSV files.
+
+        Options:
+
+            (1)
+                [filename]  the Excel file
+                [sheet]     the specific sheet in your Excel file
+
+            (2)
+                [filename]  the CSV file
+                [column]    the column needed to delete rows with null cells in this column
+
+            (3)
+                [filename]  the CSV file
+                [column]    the column that got checked for numbers
+
+            (4)
+                [filename]  the CSV file
+                [column]    the column in which the string will change
+                [string]    the string which will change into a number
+                [number]    the number the string will got changed into
+
+            (5)
+                [filename]  the CSV file
+                [column]    column with the max value
+
+            (6)
+                [filename]  the CSV file
+                [column]    column with all peoples' birthdays
+
+            (7)
+                [filename]  the CSV file
+                [column]    column with the value to count
+                [value]     value to count
         """)
