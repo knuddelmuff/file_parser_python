ae34788ef6ffadc57aafe35edd035a8f6382a66d
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-01 17:31:23 +0100

    added bdates and dates list

--- age.py
+++ age.py
@@ -9,14 +9,18 @@ import platform
 
 @click.command()
 @click.option("--file", help="CSV file")
-def change(file):
+@click.argument("Date_Column")
+@click.argument("Birthday_Column")
+def age(file, date_column, birthday_column):
     df = pd.read_csv(file, "r", encoding="iso-8859-1", delimiter=";", keep_default_na=False, dtype='unicode')
 
     #Info Print
     print("[Info] Calculate ages for file ({})and store into the new column V0_Age".format(file))
 
+    bdates = [bdate.replace(".","")[:2] for bdate in df[birthday_column]]
+    dates = [date.replace(".","")[:2] for date in df[date_column]]
 
 
 
 if __name__ == "__main__":
-    change()
+    age()
