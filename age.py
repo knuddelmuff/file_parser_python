148d5d7cc88c0a6b91b14f58aa8cd53b9aa96bb8
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-03 21:20:48 +0100

    BETA age

--- age.py
+++ age.py
@@ -3,8 +3,8 @@
 import click
 import pandas as pd
 from tqdm import tqdm
-import os
-import platform
+from helper import *
+import datetime
 
 
 @click.command()
@@ -17,10 +17,26 @@ def age(file, date_column, birthday_column):
     #Info Print
     print("[Info] Calculate ages for file ({})and store into the new column V0_Age".format(file))
 
-    bdates = [bdate.replace(".","")[:2] for bdate in df[birthday_column]]
-    dates = [date.replace(".","")[:2] for date in df[date_column]]
+    yearNow = datetime.datetime.now().year
 
+    bdates = [int(bdate[3:]) if len(bdate) > 0 else yearNow for bdate in df[birthday_column]]
+    dates = [int(date[-2:]) + 2000  if len(date) > 0 else yearNow for date in df[date_column]]
 
+    ages = []
+
+    for index, bdate in enumerate(bdates):
+        ages.append(dates[index] - bdate)
+
+    print(ages)
+    df["V0_Ages"] = ages
+
+    #Progress Bar:
+    for _ in tqdm(range(10000000)):
+        pass
+
+    df.to_csv("edit_{}".format(file), sep=";", encoding="iso-8859-1")
+
+    openFolder()
 
 if __name__ == "__main__":
     age()
