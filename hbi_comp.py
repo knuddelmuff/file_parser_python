f3664accfed7d39ff5876933e9983eb897a351de
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-24 19:23:20 +0100

    working

--- hbi_comp.py
+++ hbi_comp.py
@@ -9,35 +9,31 @@ import datetime
 
 @click.command()
 @click.option("--file", help="CSV file")
-@click.option("--nc", help="Needs the converter from 'on' to 1")
-@click.argument("new_column")
-@click.argument("column_list")
-# @click.argument("EIMArth")
-# @click.argument("EIMIrit")
-# @click.argument("EIMEryt")
-# @click.argument("EIMPyod")
-# @click.argument("EIMOrAp")
-# @click.argument("EIMAnaF")
-# @click.argument("EIMPerA")
-# @click.argument("EIMAOthF")
-#def hbi(file, nc,new_column, eimarth, eimirit, eimeryt, eimpyod, eimorap, eimanaf, eimpera, eimaothf):
-def hbi(file, nc, new_column, column_list):
+@click.argument("EIMArth")
+@click.argument("EIMIrit")
+@click.argument("EIMEryt")
+@click.argument("EIMPyod")
+@click.argument("EIMOrAp")
+@click.argument("EIMAnaF")
+@click.argument("EIMPerA")
+@click.argument("EIMAOthF")
+def hbiComp(file, eimarth, eimirit, eimeryt, eimpyod, eimorap, eimanaf, eimpera, eimaothf):
     df = pd.read_csv(file, "r", encoding="iso-8859-1",
                      delimiter=";", keep_default_na=False, dtype='unicode')
 
     # Info Print
     print("[Info] Calculate Harvey-Bradshaw-Index (HBI) for file ({}) and store into the new column V0_HBI".format(file))
 
-    newDf = df[column_list]
+    newDf = df[[eimarth, eimirit, eimeryt, eimpyod,
+                eimorap, eimanaf, eimpera, eimaothf]]
 
-    if nc:
-        for column in newDf:
-            newDf[column] = newDf[column].replace("on", 1)
-            newDf[column] = newDf[column].replace("", 0)
+    for column in newDf:
+        newDf[column] = newDf[column].replace("on", 1)
+        newDf[column] = newDf[column].replace("", 0)
 
     counts = newDf.sum(axis=1)
 
-    df.insert(1, new_column, counts)
+    df.insert(1, "V0_EIMSum", counts)
 
     # Progress Bar:
     for _ in tqdm(range(10000000)):
@@ -49,4 +45,4 @@ def hbi(file, nc, new_column, column_list):
 
 
 if __name__ == "__main__":
-    hbi()
+    hbiComp()
