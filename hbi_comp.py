e15174f0294777fc8c1ce58d15b881ca4699a735
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-24 19:09:07 +0100

    try to add list to arguments

--- hbi_comp.py
+++ hbi_comp.py
@@ -9,31 +9,35 @@ import datetime
 
 @click.command()
 @click.option("--file", help="CSV file")
-@click.argument("EIMArth")
-@click.argument("EIMIrit")
-@click.argument("EIMEryt")
-@click.argument("EIMPyod")
-@click.argument("EIMOrAp")
-@click.argument("EIMAnaF")
-@click.argument("EIMPerA")
-@click.argument("EIMAOthF")
-def hbi(file, eimarth, eimirit, eimeryt, eimpyod, eimorap, eimanaf, eimpera, eimaothf):
+@click.option("--nc", help="Needs the converter from 'on' to 1")
+@click.argument("new_column")
+@click.argument("column_list")
+# @click.argument("EIMArth")
+# @click.argument("EIMIrit")
+# @click.argument("EIMEryt")
+# @click.argument("EIMPyod")
+# @click.argument("EIMOrAp")
+# @click.argument("EIMAnaF")
+# @click.argument("EIMPerA")
+# @click.argument("EIMAOthF")
+#def hbi(file, nc,new_column, eimarth, eimirit, eimeryt, eimpyod, eimorap, eimanaf, eimpera, eimaothf):
+def hbi(file, nc, new_column, column_list):
     df = pd.read_csv(file, "r", encoding="iso-8859-1",
                      delimiter=";", keep_default_na=False, dtype='unicode')
 
     # Info Print
     print("[Info] Calculate Harvey-Bradshaw-Index (HBI) for file ({}) and store into the new column V0_HBI".format(file))
 
-    newDf = df[[eimarth, eimirit, eimeryt, eimpyod,
-                eimorap, eimanaf, eimpera, eimaothf]]
+    newDf = df[column_list]
 
-    for column in newDf:
-        newDf[column] = newDf[column].replace("on", 1)
-        newDf[column] = newDf[column].replace("", 0)
+    if nc:
+        for column in newDf:
+            newDf[column] = newDf[column].replace("on", 1)
+            newDf[column] = newDf[column].replace("", 0)
 
     counts = newDf.sum(axis=1)
 
-    df.insert(1, "V0_EIMSum", counts)
+    df.insert(1, new_column, counts)
 
     # Progress Bar:
     for _ in tqdm(range(10000000)):
