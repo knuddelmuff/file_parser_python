cf37b59336a97b6443872e04f5232668217ecea2
teusz.alexander@gmail.com <teusz.alexander@gmail.com>
2019-03-24 19:14:21 +0100

    initial

--- HBI.py
+++ HBI.py
@@ -9,36 +9,23 @@ import datetime
 
 @click.command()
 @click.option("--file", help="CSV file")
-@click.argument
-@click.argument
-@click.argument
-@click.argument("Date_Column")
-@click.argument("Birthday_Column")
-def hbi(file, date_column, birthday_column):
+@click.argument("new_file")
+def statistics(file, date_column, birthday_column):
     df = pd.read_csv(file, "r", encoding="iso-8859-1", delimiter=";", keep_default_na=False, dtype='unicode')
 
     #Info Print
-    print("[Info] Calculate Harvey-Bradshaw-Index (HBI) for file ({}) and store into the new column V0_HBI".format(file))
+    print("[Info] Calculate Statistics for file ({}) and store into the new file statistics_{}".format(file,file))
 
-    #yearNow = datetime.datetime.now().year
 
-    bdates = [int(bdate[3:]) if len(bdate) > 0 else yearNow for bdate in df[birthday_column]]
-    dates = [int(date[-2:]) + 2000  if len(date) > 0 else yearNow for date in df[date_column]]
 
-    ages = []
-
-    for index, bdate in enumerate(bdates):
-        ages.append(dates[index] - bdate)
-
-    df.insert(8, "V0_Age", ages)
 
     #Progress Bar:
     for _ in tqdm(range(10000000)):
         pass
 
-    df.to_csv("edit_{}".format(file), sep=";", encoding="iso-8859-1")
+    df.to_csv("statistics_{}".format(file), sep=";", encoding="iso-8859-1")
 
     openFolder()
 
 if __name__ == "__main__":
-    age()
+    statistics()
